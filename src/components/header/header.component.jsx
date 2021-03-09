faeab45507c00913615ed879812ebd736bb68859
TonyHomsi <tonyhomsi87@gmail.com>
2021-03-09 14:39:59 +0100

    Hidding cart-icon once you checkout

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -40,7 +40,7 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = (state) => createStructuredSelector({
+const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   hidden: selectCartHidden
 });
