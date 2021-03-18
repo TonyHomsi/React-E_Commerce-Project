155ea3142ba8c663bc53f77b5ab93b9127435c4e
TonyHomsi <tonyhomsi87@gmail.com>
2021-03-18 14:05:20 +0100

    adding strip button component

---
+++ src/components/stripe-button/strip-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IWL0RL5g0AoNZ4KDEPSF8NJiH4lj6bgRaqPBcFzA49ACYRq5RO2qSmnIwbLJAHN8E2n8010yYklvAxmLi1QGH2P0087yGr7WN';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
