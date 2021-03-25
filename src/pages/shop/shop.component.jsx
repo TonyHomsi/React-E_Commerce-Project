f7399fd6368de66aab0582ad554450b4c590e8df
TonyHomsi <tonyhomsi87@gmail.com>
2021-03-25 10:47:54 +0100

    Adding WithSpinner Component and Using Higher Order Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,13 +5,24 @@ import { connect } from "react-redux";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
+
+
 import {
   firestore,
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
 import { updateCollections } from "../../redux/shop/shop.action";
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
+
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  }
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -22,18 +33,20 @@ class ShopPage extends React.Component {
       async (snapshot) => {
         const collectionMap = convertCollectionsSnapshotToMap(snapshot);
         updateCollections(collectionMap);
+        this.setState({ loading: false})
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const {loading} = this.state
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>}
         />
       </div>
     );
