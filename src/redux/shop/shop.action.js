1122f3a372233da0e22340c8b8bb26c877804e64
TonyHomsi <tonyhomsi87@gmail.com>
2021-03-26 09:39:35 +0100

    Adding Redux thunk & Modifying other files accordingly

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,6 +1,30 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap,
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
+
+
+export const FetchCollectionsSuccess = collectionMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionMap
+})
+
+export const FetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () =>{
+  return dispatch => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart())
+
+    collectionRef.get().then(snapshot => {
+      const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionsStart(collectionMap))
+  }).catch(error => dispatch(FetchCollectionsFailure(error.message)))
+  }
+}
