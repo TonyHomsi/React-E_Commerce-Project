f8bc56d9dd1078f4741002eeb365285e2364c779
TonyHomsi <tonyhomsi87@gmail.com>
2021-04-09 11:06:47 +0200

    Refactering code according to Saga-user

--- src/App.js
+++ src/App.js
@@ -10,19 +10,13 @@ import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import CheckoutPage from "./pages/checkout/checkout.component.jsx";
 
-import {
-  auth,
-  createUserProfileDocument,
-} from "./firebase/firebase.utils";
-import { setCurrentUser } from "./redux/user/user.actions";
+
 import { selectCurrentUser } from "./redux/user/user.selectors";
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    /* this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapshot) => {
@@ -35,8 +29,8 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth);
       }
-    });
-  }
+    });*/
+  } 
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -70,8 +64,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,22 @@
-import {UserActionType} from './user.types'
+import UserActionType from './user.types'
 
-export const setCurrentUser = user =>({
-    type: UserActionType.SET_CURRENT_USER,
+export const googleSignInStart = () =>({
+    type: UserActionType.GOOGLE_SIGN_IN_START
+})
+
+export const signInSuccess = user =>({
+    type: UserActionType.SIGN_IN_SUCCESS,
     payload: user
+})
 
+
+export const signInFailure = error =>({
+    type: UserActionType.SIGN_IN_FAILURE,
+    payload: error
+})
+
+
+export const emailSignInStart = emailAndPassword =>({
+    type: UserActionType.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword
 })
